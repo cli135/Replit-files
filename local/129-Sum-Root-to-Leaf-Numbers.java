8252843d0a6b577253537bb79e98cd41da16a44d
Christopher Li <cli135@jhu.edu>
2022-03-25 04:01:52 -0400

    add cpp

--- local/129-Sum-Root-to-Leaf-Numbers.java
+++ local/129-Sum-Root-to-Leaf-Numbers.java
@@ -96,3 +96,75 @@ class Solution {
         return sumNumbers(root.left, before * 10 + root.val) + sumNumbers(root.right, before * 10 + root.val);
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/**
+//cpp solution below
+
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        // need the heap dynamic memory
+        // to make sure callee changes
+        // are seen in caller functions
+        // and not lost when call stack popped
+        // upon returning.
+        // heap kind of is like global variable
+        // since otherwise using like an int
+        // copy/pass by value would just lose
+        // any changes made in the callee
+        // unless it was specifically returned and i guess
+        // that might work but still what if you need
+        // to return more than one sum or sthg i guess like
+        // it might just be easier to pass around a vector
+        // or just a integer on the stack idk my thought lol
+        vector<int> nums = {};
+        sumThisPath(root, root != nullptr ? root->val : 0, nums);
+        int sum = 0;
+        for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
+            sum += *it;
+            cout << *it << endl;
+        }
+        return sum;
+    }
+    // depth first search
+    // void functions which
+    // modify heap memory is like
+    // having a global variable
+    // which is harder to debug
+    // but i like it
+    // ok i need to change to functional programming then
+    void sumThisPath(TreeNode* root, int runningSum, vector<int>& nums) {
+        // base case
+        // end of path: storing the accumulated sum along this path 
+        bool leaf = root->left == nullptr && root->right == nullptr;
+        if (leaf) {
+            nums.push_back(runningSum);
+            return;
+        }
+        
+        // recursive case
+        // summing along the path, doing the * 10 make the place values align
+        // and doing things a half step early rather than
+        // summing root->val on time
+        if (root->left != nullptr) {
+            sumThisPath(root->left, runningSum * 10 + root->left->val, nums);
+        }
+        if (root->right != nullptr) {
+            sumThisPath(root->right, runningSum * 10 + root->right->val, nums);
+        }
+    }
+};
+ */
